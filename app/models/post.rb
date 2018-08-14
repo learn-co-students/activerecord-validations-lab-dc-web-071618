d87311bb485907ff5d3696e99de3fbdc1fe4e814
github username <github email address>
2018-08-14 15:00:56 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :title, :is_clickbaity
+end
+
+def is_clickbaity
+  clickbaits = ["Won't Believe", "Secret", "Top", "Guess"]
+  if title
+    check = clickbaits.map do |clickbait|
+      title.include?(clickbait)
+    end
+  end
+  if check && check.exclude?(true)
+    errors.add(:title, "not clickbait-y")
+  end
 end
